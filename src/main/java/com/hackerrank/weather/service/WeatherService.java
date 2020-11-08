c85a6bf73f85f3447ea7a1d694289b5af1064d93
hangpei-bazahei <allen.hangpei@gmail.com>
2020-11-07 20:28:50 -0500

    update repo

--- src/main/java/com/hackerrank/weather/service/WeatherService.java
+++ src/main/java/com/hackerrank/weather/service/WeatherService.java
@@ -1,9 +1,16 @@
 package com.hackerrank.weather.service;
 
+import com.hackerrank.weather.model.Location;
 import com.hackerrank.weather.model.Weather;
 
+import java.util.Date;
 import java.util.List;
 
 public interface WeatherService {
-    void uploadWeatherData(List<Weather> weather);
+    public void uploadWeatherData(List<Weather> weather);
+    public List<Weather> getWeatherByLocation(Location location);
+    public void deleteAllWeather();
+    public void deleteWeatherByDateAndLocation(Location location, Date date);
+    public void getAllWeather();
+
 }
--- src/main/java/com/hackerrank/weather/service/WeatherServiceImpl.java
+++ src/main/java/com/hackerrank/weather/service/WeatherServiceImpl.java
@@ -1,10 +1,12 @@
 package com.hackerrank.weather.service;
 
+import com.hackerrank.weather.model.Location;
 import com.hackerrank.weather.model.Weather;
 import com.hackerrank.weather.repository.WeatherRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -16,5 +18,25 @@ public class WeatherServiceImpl implements WeatherService {
     @Override
     public void uploadWeatherData(List<Weather> weather) {
         weatherRepository.save(weather);
+    }
+
+    @Override
+    public void deleteWeatherByDateAndLocation(Location location, Date date) {
+
+    }
+
+    @Override
+    public List<Weather> getWeatherByLocation(Location location) {
+        return null;
+    }
+
+    @Override
+    public void deleteAllWeather() {
+
+    }
+
+    @Override
+    public void getAllWeather() {
+
     }
 }
